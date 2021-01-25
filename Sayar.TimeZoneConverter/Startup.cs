8322ee34d5592c26a747578e64e3aaf8e7ff8e83
sayarshakya <sakyashire@hotmail.com>
2021-01-25 15:11:59 +0545

    development mode changes

--- Sayar.TimeZoneConverter/Startup.cs
+++ Sayar.TimeZoneConverter/Startup.cs
@@ -34,17 +34,19 @@ namespace Sayar.TimeZoneConverter
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseDatabaseErrorPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
